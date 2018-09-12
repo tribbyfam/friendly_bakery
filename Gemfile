8cd22a6bd704cdeb14c95991905e2f664926c4a9
Mac <mac@MacBook-Pro.fios-router.home>
2018-09-11 20:29:38 -0500

    set up initial app ruby file with gemfile

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org/"
+
+gem 'sinatra'
+gem 'materialize', '~> 0.4.5'
+gem 'material_icons', '~> 2.2', '>= 2.2.1'
+gem 'httparty', '~> 0.16.2'
---
+++ app.rb
@@ -0,0 +1,20 @@
+require 'sinatra'
+
+
+get '/' do
+  "hello my bakery"
+end
+
+get '/cookies' do
+  "hello my cookie page"
+end
+
+
+get '/cakes' do
+  "hello my cakes page"
+end
+
+get '/muffins' do
+  "hello my muffins page"
+end
+
