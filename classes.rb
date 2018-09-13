e4fb78035356758f53665c1efd2fdfae5b61fa78
Mac <mac@MacBook-Pro.local>
2018-09-13 11:56:11 -0500

    worked on nav bar, added favicon and logo, need to style it

--- classes.rb
+++ classes.rb
@@ -8,13 +8,20 @@ class AllGoods
     @title = title
     @description = description
     @price = price
-    @picture = picture
+    @image = image
+  end
+
+  def display
+    @title
+    @description
+    @price
+    @image
   end
 
   # def display
   #   goods.each do |item|
   #     define_method("#{goods}_name") do |arg|
-  #       puts "Name: #{goods}"
+  #       puts "Name: #{goods.title}"
   #       puts arg
   #     end
   #   end
@@ -23,6 +30,8 @@ end
 
 my_cookie = AllGoods.new(title: "Chocolate",description: "Out of this world chocolaty heaven", price: "$29.99", image: "/images/cake1.jpeg")
 puts my_cookie.inspect
+
+
 # puts goods.cakes_name(title: "Chocolate","Out of this world chocolaty heaven")
 
 class Cookies < AllGoods
